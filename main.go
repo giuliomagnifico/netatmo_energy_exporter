12efca6de948f5b7f3ffaa86ad85003140b7343a
giuliomagnifico <giuliomagnifico@gmail.com>
2025-04-04 12:17:33 +0200

    Fixed a bug when rebooted
    Now it keeps the token file

--- main.go
+++ main.go
@@ -12,80 +12,73 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/version"
-
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	netatmo "github.com/tipok/netatmo_exporter/netatmo-api"
 )
 
+const tokenFile = "netatmo_token.json"
+
+func saveToken(token string) error {
+	return os.WriteFile(tokenFile, []byte(token), 0600)
+}
+
+func loadToken() (string, error) {
+	data, err := os.ReadFile(tokenFile)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
 func main() {
-	var clientID string
-	var clientSecret string
-	var username string
-	var password string
-	var listen string
-	var refreshToken string
+	var clientID, clientSecret, listen string
 	flag.StringVar(&clientID, "client-id", "", "Netatmo API client ID")
 	flag.StringVar(&clientSecret, "client-secret", "", "Netatmo API client secret")
-	flag.StringVar(&username, "username", "", "Netatmo username")
-	flag.StringVar(&password, "password", "", "Netatmo password")
-	flag.StringVar(&refreshToken, "refresh-token", "", "Netatmo refresh-token")
 	flag.StringVar(&listen, "listen", ":2112", "Address to listen on")
 	flag.Parse()
 
-	if clientID == "" {
-		log.Fatal("Netatmo API client ID has to be provided.")
+	if clientID == "" || clientSecret == "" {
+		log.Fatal("Netatmo API client ID and secret must be provided.")
 	}
 
-	if clientSecret == "" {
-		log.Fatal("Netatmo API client secret has to be provided.")
+	refreshToken, err := loadToken()
+	if err != nil || refreshToken == "" {
+		log.Fatalf("Could not load refresh token from %s: %v", tokenFile, err)
 	}
 
-	refreshTokenUsed := false
-	if refreshToken != "" {
-		refreshTokenUsed = true
-	}
-
-	if username == "" && !refreshTokenUsed {
-		log.Fatal("Netatmo username has to be provided.")
-	}
-
-	if password == "" && !refreshTokenUsed {
-		log.Fatal("Netatmo password has to be provided.")
-	}
-
-	prometheus.MustRegister(version.NewCollector("netatmo_exporter"))
-
-	cnf := &netatmo.Config{
+	config := &netatmo.Config{
 		ClientID:     clientID,
 		ClientSecret: clientSecret,
-		Username:     username,
-		Password:     password,
 		RefreshToken: refreshToken,
 		Scopes:       []string{netatmo.ReadStation, netatmo.ReadThermostat},
 	}
-	client, err := netatmo.NewClient(context.Background(), cnf)
+
+	client, err := netatmo.NewClient(context.Background(), config)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Salvataggio automatico del nuovo token aggiornato
+	if client.Config.RefreshToken != refreshToken {
+		if err := saveToken(client.Config.RefreshToken); err != nil {
+			log.Printf("Error saving new token: %v", err)
+		} else {
+			log.Println("New refresh token saved.")
+		}
+	}
+
+	prometheus.MustRegister(version.NewCollector("netatmo_exporter"))
 	collector := newCollector(client)
 	prometheus.MustRegister(collector)
 
 	sig := make(chan os.Signal, 1)
-	signal.Notify(
-		sig,
-		syscall.SIGTERM,
-		syscall.SIGINT,
-	)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
 	defer signal.Stop(sig)
 
 	mux := http.NewServeMux()
 	mux.Handle("/metrics", promhttp.Handler())
 
-	srv := &http.Server{
-		Addr:    listen,
-		Handler: mux,
-	}
+	srv := &http.Server{Addr: listen, Handler: mux}
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil {
